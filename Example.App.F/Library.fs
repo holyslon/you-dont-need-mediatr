159e968f7607242cc91920179423b955e035882f
Anton Onikiychuk <a.onikiychuk@dodopizza.com>
2022-10-24 16:02:09 +0200

    Add f#

--- Example.App.F/Library.fs
+++ Example.App.F/Library.fs
@@ -1,9 +1,15 @@
 ﻿namespace Example.App.F
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Calculator =
         
-    let validator = 
+    let fibonacciFormula state = Some(fst state + snd state, (snd state, fst state + snd state))
         
+    let fibonacciSeq = (0,1) |> Seq.unfold fibonacciFormula
     
+    let factorialFormula ((number, value): int * int) = Some(value, (number + 1, value * (number + 1)))
+    
+    let factorialSeq = (0,1) |> Seq.unfold factorialFormula
+    
+    let Calculate num = 
+        Some(fibonacciSeq |> Seq.take num |> Seq.last, factorialSeq |> Seq.take num |> Seq.last)
+        
---
+++ Example.Web/Controllers/FSharpController.cs
@@ -0,0 +1,31 @@
+﻿using Example.App.Native;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Web.Controllers;
+
+[ApiController]
+[Route("fsharp")]
+public class FSharpController : ControllerBase
+{
+    private readonly CalculationUnit _unit;
+
+    public FSharpController(CalculationUnit unit)
+    {
+        _unit = unit;
+    }
+    
+    public record CalculateInput(int? target){}
+
+    [HttpPost("calculate")]
+    public async Task<(int,int)> Calculate([FromBody] CalculateInput input)
+    {
+        if (!input.target.HasValue)
+        {
+            throw new BadHttpRequestException($"{nameof(MediatRController.CalculateInput.target)} should be present", 400);
+        }
+        else
+        {
+            return App.F.Calculator.Calculate(input.target.Value).Value.ToValueTuple();
+        }
+    }
+}
